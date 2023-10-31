fef171c8af4242596573756f36626bad16ef1bc2
Ron <81550623+thisellie@users.noreply.github.com>
2023-10-31 17:04:13 +0800

    Add function to include forecast data

--- src/script.js
+++ src/script.js
@@ -18,41 +18,66 @@ const gust = document.getElementById('gust')
 const start = document.getElementById('start')
 const current = document.getElementById('current')
 const notif = document.getElementById('notification')
+const forecast = document.getElementById('forecast')
+const days = Array.from(document.getElementsByClassName('day'))
 
 const key = '4eb1171f375141c7a8a122848231010'
 
 async function getWeather(city) {
-  const query = `https://api.weatherapi.com/v1/current.json?key=${key}&q=${city}`
-  const response = await fetch(query)
+  const call = `https://api.weatherapi.com/v1/forecast.json?key=${key}&q=${city}&days=3`
+  const response = await fetch(call)
   return await response.json()
 }
 
-function timeFormat(text) {
+function timeFormat(input) {
   const formatter = new Intl.DateTimeFormat('en-US', {
     hour: '2-digit',
     minute: '2-digit',
     hour12: true,
   })
-  const parsed = new Date(text)
+  const parsed = new Date(input)
   return formatter.format(parsed)
+}
+
+function dateFormat(input) {
+  const date = new Date(input)
+  const week = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+  const index = date.getDay()
+  const day = date.getDate()
+  return `${week[index]} ${day}`
 }
 
 function displayWeather(weather) {
   title.textContent = `${weather.location.name}, ${weather.location.country}`
   time.textContent = `${timeFormat(weather.location.localtime)}`
-  temperature.textContent = `${weather.current.temp_c}°C`
+  temperature.textContent = `${weather.current.temp_c}°`
   overview.textContent = weather.current.condition.text
   cloud.src = weather.current.condition.icon
-  feels.textContent = `Feels like ${weather.current.feelslike_c}°C`
+  feels.textContent = `Feels like ${weather.current.feelslike_c}°`
   wind.textContent = `${weather.current.wind_kph} km/h`
   pressure.textContent = `${weather.current.pressure_mb} mb`
   precip.textContent = `${weather.current.precip_mm} mm`
   humid.textContent = `${weather.current.humidity}%`
   coverage.textContent = `${weather.current.cloud}%`
   gust.textContent = `${weather.current.gust_kph} km/h`
+
+  days.forEach((day, index) => {
+    day.children[1].children[0].src =
+      weather.forecast.forecastday[index].day.condition.icon
+    day.children[1].children[1].children[0].textContent = `${weather.forecast.forecastday[index].day.maxtemp_c}°`
+    day.children[1].children[1].children[1].textContent = `${weather.forecast.forecastday[index].day.mintemp_c}°`
+    if (index === 0) {
+      day.children[1].children[2].children[0].textContent = `${weather.forecast.forecastday[index].day.condition.text}`
+      day.children[1].children[2].children[1].textContent = `${weather.forecast.forecastday[index].day.daily_chance_of_rain}%`
+    }
+    if (index > 0)
+      day.children[0].textContent = dateFormat(
+        weather.forecast.forecastday[index].date
+      )
+  })
 }
 
-async function loadAnimation() {
+async function loadWeather() {
   notif.style.display = 'flex'
   const data = await getWeather(search.value)
   notif.style.display = 'none'
@@ -62,10 +87,11 @@ async function loadAnimation() {
 form.addEventListener('keypress', async e => {
   if (e.key === 'Enter') {
     e.preventDefault()
-    const weather = await loadAnimation()
+    const weather = await loadWeather()
     displayWeather(weather)
     start.style.display = 'none'
     current.style.display = 'block'
+    forecast.style.display = 'block'
     form.reset()
   }
 })
